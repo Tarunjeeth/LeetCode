f66641714c37333bdb077e63cb9659fcb4416f18
tarunjeeth <75776492+Tarunjeeth@users.noreply.github.com>
2026-07-26 19:10:40 +0530

    Time: 0 ms (100%), Space: 26.4 MB (80.2%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -2,24 +2,27 @@ class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
         int n=grid.size();
-
+        long long n2=n*n;
         vector <int> array(n*n,0);
         vector <int> out(2,0);
+        long long expected_sum=n2*(n2+1)/2;
+        long long expected_sq_sum=n2*(n2+1)*(2*n2+1)/6;
+        int actual_sum=0;
+        long long actual_sq_sum=0;
+        
         for (int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if (array[grid[i][j]-1]!=0){
-                    out[0]=grid[i][j];
-                }
-                else{
-                    array[grid[i][j]-1]++;
-                }
+                actual_sq_sum+=(grid[i][j])*(grid[i][j]);
+                actual_sum+=(grid[i][j]);
             }
         }
-        for (int i=0;i<n*n;i++){
-            if (array[i]==0){
-                out[1]=i+1;
-            }
-        }
+        int c1=actual_sum-expected_sum;
+        int c2=actual_sq_sum-expected_sq_sum;
+        int c3=c2/c1;
+        int miss=(c3-c1)/2;
+        int repeat=c3-miss;
+        out[0]=repeat;
+        out[1]=miss;
         return out;
     }
 };
